93b8ef7c40e5627b73933383d6cbfb970919bf9b
AllyasErik <erik.vasarhelyi@yahoo.com>
2020-12-10 20:27:03 +0100

    Drill

---
+++ source/Ch17/drill17-2.cpp
@@ -0,0 +1,54 @@
+#include "std_lib_facilities.h"
+
+ostream& print_array(ostream& os, int* a, int n)
+{
+	for(int i = 0; i < n; ++i)
+		os << a[i] << endl;
+	return os;
+}
+
+int main()
+{
+	int a = 7;
+	int *p1 = &a;
+	cout << "the value of the p1 pointer: " << p1 << " and p1 pointer points on the following element: " << *p1 << endl; 
+	cout << endl;
+
+	int *p2 = new int[7] {1, 2, 4, 8, 16, 32, 64};
+	cout << "the value of the p2 pointer: " << p2 << endl;
+	cout << "the value of the p2 pointer: " << &p2[1] << endl;
+	for(int i=0; i<7; ++i)
+		cout << p2[i] << endl;
+	cout << endl;
+
+	int* p3 = p2;
+	p2 = p1;
+	p2 = p3;
+	cout << "The value of the p1 pointer: " << p1 << " and p1 pointer points on the following element: " << *p1 << endl; 
+	cout << "The value of the p2 pointer: " << p2 << " and p2 pointer points on the following element: " << *p2 << endl; 
+	cout << endl;
+
+	delete[] p2;
+
+	cout << "Cout after the copy\n";
+	p1 = new int[10] {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+	p2 = new int[10];
+	for(int i = 0; i < 10; ++i)
+		p2[i]=p1[i];
+	for(int i = 0; i < 10; ++i)
+		cout << p2[i] << endl;
+	delete[] p2;
+	delete[] p1;
+	cout << endl;
+
+	cout << "vector cout\n";
+	vector<int> v1 {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+	vector<int> v2(10);
+	v2=v1;
+	for(auto v : v2)
+		cout << v << endl;
+	cout << endl;
+
+
+	return 0;
+}
